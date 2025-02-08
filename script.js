4db1e37bf7bd4b4ed504f41998380d41c1796a56
Paul <69437294+Escbrk@users.noreply.github.com>
2025-02-07 22:24:34 -0700

    added notification "unsopported devices"

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
-const yes = document.querySelector("#yes");
-const no = document.querySelector("#no");
-const container = document.querySelector(".movable-container");
-const input = document.querySelector("#input");
-const agree = document.querySelector("#agree");
+const ELEMENTS = {
+  yes: document.getElementById("yes"),
+  no: document.getElementById("no"),
+  container: document.querySelector(".container"),
+  agree: document.getElementById("agree"),
+};
 
-no.addEventListener("mouseover", () => {
-  const maxX = window.innerHeight - container.offsetHeight;
-  const maxY = window.innerWidth - container.offsetWidth;
+const windowWidth = window.innerWidth;
+
+if (windowWidth < 1220) {
+  ELEMENTS.container.innerHTML =
+    "<h1>MOBILE DEVICES ARE NOT SUPPORTED PLEASE USE A LAPTOP</h1>";
+  ELEMENTS.container.style.backgroundColor = "black";
+  ELEMENTS.container.style.textAlign = "left";
+
+  document.body.style.backgroundImage = "none";
+  document.body.style.backgroundColor = "black";
+}
+
+ELEMENTS.no.addEventListener("mouseover", () => {
+  const maxX = window.innerHeight - ELEMENTS.container.offsetHeight;
+  const maxY = window.innerWidth - ELEMENTS.container.offsetWidth;
 
   const x = Math.floor(Math.random() * maxX);
   const y = Math.floor(Math.random() * (maxY - 300) + 300);
 
-  container.style.left = `${y}px`;
-  container.style.top = `${x}px`;
+  ELEMENTS.container.style.left = `${y}px`;
+  ELEMENTS.container.style.top = `${x}px`;
 });
 
-yes.addEventListener("click", () => {
-  container.classList.add("visually-hidden");
-  container.classList.remove("movable-container");
+ELEMENTS.yes.addEventListener("click", () => {
+  ELEMENTS.container.classList.add("visually-hidden");
+  ELEMENTS.container.classList.remove("container");
 
-  agree.classList.remove("visually-hidden");
-  agree.classList.add("emoji");
+  ELEMENTS.agree.classList.remove("visually-hidden");
+  ELEMENTS.agree.classList.add("emoji");
 });
