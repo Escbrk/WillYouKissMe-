46aa030c294af0b923bd8c14508bc6d4e042422b
Paul <69437294+Escbrk@users.noreply.github.com>
2025-02-07 23:19:35 -0700

    added thai language

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const windowWidth = window.innerWidth;
 
 if (windowWidth < 1220) {
   ELEMENTS.container.innerHTML =
-    "<h1>MOBILE DEVICES ARE NOT SUPPORTED PLEASE USE A LAPTOP</h1>";
+    "<h1>อุปกรณ์มือถือไม่ได้รับการสนับสนุนโปรดใช้แล็ปท็อป</h1>";
   ELEMENTS.container.style.backgroundColor = "black";
   ELEMENTS.container.style.textAlign = "left";
 
